9d94188e3b8b45436621504eb72e2261c3ce3e19
Danylo Zymbalevskyi <zymbalevskyy@gmail.com>
2023-06-14 12:31:33 +0300

    completed

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Add your code here
+function submitData(name, email) {
+  const url = "http://localhost:3000/users";
+  const data = {
+    name: name,
+    email: email,
+  };
+
+  const configurationObject = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+    },
+    body: JSON.stringify(data),
+  };
+
+  return fetch(url, configurationObject)
+    .then((response) => response.json())
+    .then((json) => {
+      const id = json.id;
+      const p = document.createElement("p");
+      p.textContent = id;
+      document.body.appendChild(p);
+    })
+    .catch((error) => {
+      const p = document.createElement("p");
+      p.textContent = error.message;
+      document.body.appendChild(p);
+    });
+}
